fad352c6f0d08e6a32e7c7a9f26d76ad533825d6
Lepri <leprikaka@gmail.com>
2023-01-15 19:00:22 +0300

    add bullet

--- Alien_Invasion.py
+++ Alien_Invasion.py
@@ -2,6 +2,7 @@ import pygame
 from settings import Settings
 from ship import Ship
 import game_functions as gf
+from pygame.sprite import Group
 
 
 def run_game():
@@ -11,11 +12,18 @@ def run_game():
     screen = pygame.display.set_mode((ai_settings.screen_wigth, ai_settings.screen_height))
     pygame.display.set_caption("Alien Invasion")
     ship = Ship(ai_settings, screen)  # Создаем корабль
+    bullets = Group()  # Создание группы для хранения пуль.
 
     while True:
-        gf.check_events(ship)
+        gf.check_events(ai_settings, screen, ship, bullets)
         ship.update()
-        gf.update_screen(ai_settings, screen, ship)
+        bullets.update()
+        gf.update_screen(ai_settings, screen, ship, bullets)
+
+        # Удаление старых пуль
+        for bullet in bullets.copy():
+            if bullet.rect.bottom <= 0:
+                bullets.remove(bullet)
 
 
 run_game()
--- settings.py
+++ settings.py
@@ -10,3 +10,9 @@ class Settings():
 
         # настройки корабля
         self.ship_speed_factor = 1.5
+
+        # параметры пули
+        self.bullet_speed_factor = 1
+        self.bullet_wight = 3
+        self.bullet_height = 15
+        self.bullet_color = (60, 0, 0)
